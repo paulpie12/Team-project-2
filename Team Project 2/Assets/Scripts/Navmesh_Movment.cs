403abd7a3348931d0f0f576eee767ae1cbfb1b1f
sh814877 <sh814877@ucf.edu>
2024-02-26 00:07:30 -0500

    Distraction

--- Team Project 2/Assets/Scripts/Navmesh_Movment.cs
+++ Team Project 2/Assets/Scripts/Navmesh_Movment.cs
@@ -7,16 +7,32 @@ public class Navmesh_Movment : MonoBehaviour
 {
     public NavMeshAgent Enemy;
     public Transform Player;
+    public Transform Distraction;
+
+    
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
-        Enemy.SetDestination(Player.position);
+        if (Throwable.doesDistractionExist == false)
+        {
+            Enemy.SetDestination(Player.position);
+            Debug.Log("Donut is targeting the player");
+        }
+        
+        else
+        {
+            Enemy.SetDestination(Distraction.position);
+            Debug.Log("Donut is targeting a distraction object");
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Entered collision with " + collision.gameObject.name);
     }
 }
