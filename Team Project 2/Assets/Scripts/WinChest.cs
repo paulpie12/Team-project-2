3fcf00d51ceea622f6b11d67797f58e8adfd7a00
LuddyJ <je864952@ucf.edu>
2024-03-03 01:25:29 -0500

    wrow

--- Team Project 2/Assets/Scripts/WinChest.cs
+++ Team Project 2/Assets/Scripts/WinChest.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class WinChest : MonoBehaviour
+
+
 {
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        animator.SetFloat("Blend", 0);
+        animator.SetFloat("MoveY", 0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
         {
+            animator.SetFloat("Blend", 1);
+            animator.SetFloat("MoveY", 1);
             Invoke("WinMenu", 1);
         }
     }
