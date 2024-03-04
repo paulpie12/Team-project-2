af3b3deac0a5c70bb7783a6f99ce2418e89a4c82
LuddyJ <je864952@ucf.edu>
2024-03-03 22:49:00 -0500

    last

--- Team Project 2/Assets/Scripts/stun.cs
+++ Team Project 2/Assets/Scripts/stun.cs
@@ -4,17 +4,70 @@ using UnityEngine;
 
 public class stun : MonoBehaviour
 {
-    public Transform bulletSpawnPoint;
-    public GameObject BulletPrefab;
-    public float bulletSpeed = 10;
+    [Header("References")]
+    public Transform cam;
+    public Transform attackPoint;
+    public GameObject objectToThrow;
+
+    [Header("Settings")]
+    public int totalThrows;
+    public float throwCooldown;
+
+    [Header("Throwing")]
+    public KeyCode throwKey = KeyCode.F;
+    public float throwForce;
+    public float throwUpwardForce;
+
+    bool readyToThrow;
+
+    private void Start()
+    {
+        readyToThrow = true;
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+         if(Input.GetKeyDown(throwKey) && readyToThrow && totalThrows > 0) 
         {
-            var bullet = Instantiate(BulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+            Throw();
         }
     }
 
+    private void Throw()
+    {
+        readyToThrow = false;
+
+        //This instantiates and object to throw
+        GameObject projectile = Instantiate(objectToThrow, attackPoint.position, cam.rotation);
+        
+        Destroy(projectile, 5);
+
+        //get rigidbody component
+        Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
+
+        //calculate direction
+        Vector3 forceDirection = cam.transform.forward;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(cam.position, cam.forward, out hit, 500f))
+        {
+            forceDirection = (hit.point - attackPoint.position).normalized;
+        }
+
+        //add force
+        Vector3 forceToAdd = forceDirection * throwForce + transform.up * throwUpwardForce;
+
+        projectileRb.AddForce(forceToAdd, ForceMode.Impulse);
+
+        totalThrows--;
+
+        //implement throwCooldown
+        Invoke(nameof(ResetThrow), throwCooldown);
+    }
+
+    private void ResetThrow()
+    {
+        readyToThrow = true;
+    }
 }
